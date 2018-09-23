3ace3819ab7bd382938acef1ad0f38fc6c299066
Oysumi <aurelien54.hab@orange.fr>
2018-09-23 16:19:03 +0200

    modified AbstractUnit to set pointers to the owner and a texture, I'll have to define constructors

--- v0.1/CharactersClass/AbstractUnit.h
+++ v0.1/CharactersClass/AbstractUnit.h
@@ -1,6 +1,8 @@
 #ifndef __ABSTRACTUNIT_H__
 #define __ABSTRACTUNIT_H__
 
+#include "AbstractPlayer.h"
+#include "../DisplayClass/texture.h"
 #include <string>
 
 class AbstractUnit
@@ -11,6 +13,7 @@ class AbstractUnit
 		AbstractUnit(std::string name, short int strength, unsigned short int size, unsigned short int posx, unsigned short int posy) ;
 		// virtual void display() const ; virtuelle pure ?
 		~AbstractUnit() ;
+		// define two methods to set the owner and the texture
 
 
 	protected:
@@ -20,6 +23,8 @@ class AbstractUnit
 		unsigned short int m_unit_size ;
 		unsigned short int m_unit_posx ;
 		unsigned short int m_unit_posy ;
+		Texture * m_myTexture ;
+		AbstractPlayer * m_myOwner ;
 } ;
 
 #endif // __ABSTRACTUNIT_H__
